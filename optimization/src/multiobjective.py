03bc87bfbfaf7182b0f99a9d76ebb393f03aa3c3
Kassra-sinaei <kasra.sinaei@gmail.com>
2021-08-29 14:23:32 +0430

    Constraint removed + termination criteria added

--- optimization/src/multiobjective.py
+++ optimization/src/multiobjective.py
@@ -26,14 +26,14 @@ class surena_offline(Problem):
     def __init__(self):
         super().__init__(n_var=5,
                          n_obj=2,
-                         n_constr=1,
+                         n_constr=0,
                          xl=np.array([0.2,0.1,0.5,0.65,0.025]),
                          xu=np.array([0.7,0.5,1.3,0.7,0.075]))
 
     def _evaluate(self, X, out, *args, **kwargs):
         f1 = np.empty(X.shape[0])
         f2 = np.empty_like(f1)
-        g = np.empty_like(f2)
+        #g = np.empty_like(f2)
 
         for i in range(X.shape[0]):
             rospy.wait_for_service('optimization')
@@ -42,9 +42,9 @@ class surena_offline(Problem):
             res = optimization_client(X[i,0], X[i,1], X[i,2], X[i,3], X[i,4], 5)
             f1[i] = res.j[0]
             f2[i] = res.j[1]
-            g[i] = res.j[2]
+            #g[i] = res.j[2]
         out["F"] = np.column_stack([f1, f2])
-        out["G"] = g
+        #out["G"] = g
 
 if __name__ == "__main__":
     rospy.wait_for_service('optimization')
@@ -53,16 +53,23 @@ if __name__ == "__main__":
     problem = surena_offline()
     # problem = get_problem("bnh")
 
-    algorithm = NSGA2(pop_size=50,
+    algorithm = NSGA2(pop_size=75,
                         #n_offsprings=1,
                         sampling=get_sampling("real_random"),
                         crossover=get_crossover("real_sbx", prob=0.9, eta=10),
                         mutation=get_mutation("real_pm", eta=15),
                         eliminate_duplicates=True)
-    #termination = mdt()
+    termination = mdt(x_tol=1e-8,
+        cv_tol=1e-6,
+        f_tol=0.005,
+        nth_gen=5,
+        n_last=20,
+        n_max_gen=100,
+        n_max_evals=5000)
+
     res = minimize(problem,
                 algorithm,
-                ('n_gen', 150),
+                termination,
                 seed=1,
                 verbose=True, save_history=True)
     
